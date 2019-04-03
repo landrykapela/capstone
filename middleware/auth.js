5db3b7df7aea623c41153224eac230ed673a73e7
landrykapela <=>
2019-04-03 14:17:01 +0300

    Changed random secret for jwt

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,25 +7,26 @@ module.exports = (req,res,next) =>{
 	try {
 		// console.log("getting the headers...");
 		let token = req.headers.authorization.split(" ")[1];
-		// console.log(token);
-		let decodedToken = jwt.verify(token, 'random_secret');
+		console.log(token);
+		let decodedToken = jwt.verify(token, 'secret_random');
 		// console.log("verifying token...");
 		// console.log(decodedToken);
 		let userId = decodedToken.userId;
 
-		
+
 		if(req.body.userId && req.body.userId !== userId){
 			let error = new Error("Invalid User ID");
 			console.error(error);
-			throw "Invalid User ID";
+			res.status(401).json({error:error});
 		}
 		else{
 			next();
 		}
-		
+
 	}
 	catch{
-		res.status(401).json({error: new Error("Invalid Request")});
+		console.log("Braaaaapt");
+		let error = "Invalid Request";
+		res.status(401).json({error: error});
 	}
 };
-	
