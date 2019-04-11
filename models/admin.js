e5da7a447c96afdd52c325785fc60d4febd7ad52
landrykapela <=>
2019-04-11 20:01:40 +0300

    Remove bugs in model classes

--- models/admin.js
+++ models/admin.js
@@ -3,11 +3,10 @@ const Moderator = require('./moderator');
 //admin class inherits from moderator
 class Admin extends Moderator {
 	constructor(data){
-
+		super(data);
 	}
 	editComment(id){
 		alert("Modify Comment with ID: "+id+"?");
 	}
 
 }
-export default Admin;
