a1ceca88a0a05a2e1432cf818670bbb49ca1546e
landrykapela <=>
2019-04-11 18:29:18 +0300

    Improve comments class

--- models/comment.js
+++ models/comment.js
@@ -1,17 +1,29 @@
 //class definition for comments
 class Comment{
 	constructor(data){
-		this.id = data.commentId;
-		this.message = data.message;
-		this.author = data.author;
-		this.timestamp = data.timestamp;
-		this.parent = data.parent;
-
+		this.data = data;
 	}
-
+	getId(){
+		return this.data.commentId;
+	}
+	getMessage(){
+		return this.data.message;
+	}
+	getAuthor(){
+		return this.data.author;
+	}
+	getTimestamp(){
+		return this.data.timestamp;
+	}
+	getParent(){
+		return (!this.data.parent || this.data.parent === -1) ? -1 : this.data.parent;
+	}
 	isReply(){
-		if(this.parent){
+		if(this.parent && this.parent != -1){
 			return true;
+		}
+		else{
+			return false;
 		}
 	}
 }
